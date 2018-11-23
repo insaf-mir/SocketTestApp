7e7d253668f4ef818e740c9969be8f09be91e64d
Insaf Mukhametshin <>
2018-11-23 11:07:36 +0300

    Pod install

--- Podfile
+++ Podfile
@@ -1,7 +1,14 @@
+source 'https://github.com/ABTSoftware/PodSpecs.git'
+source 'https://github.com/CocoaPods/Specs.git'
+workspace 'SocketTestApp'
+project 'SocketTestApp.xcodeproj'
+platform :ios, '11.0'
+
 inhibit_all_warnings!
 
 target 'SocketTestApp' do
     use_frameworks!
+    pod 'SciChart'
     pod 'RxSwift',    '~> 4.0'
     pod 'RxCocoa',    '~> 4.0'
     pod 'RxDataSources'
--- Pods/Target Support Files/Pods-SocketTestApp/Pods-SocketTestApp-frameworks.sh
+++ Pods/Target Support Files/Pods-SocketTestApp/Pods-SocketTestApp-frameworks.sh
@@ -148,6 +148,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxDataSources/RxDataSources.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
+  install_framework "${PODS_ROOT}/SciChart/SciChart.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Starscream/Starscream.framework"
 fi
@@ -157,6 +158,7 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxDataSources/RxDataSources.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
+  install_framework "${PODS_ROOT}/SciChart/SciChart.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Starscream/Starscream.framework"
 fi
